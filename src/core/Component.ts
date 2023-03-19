4804eeece8351aeb0110d93fa49bae3be8379880
bbakjun <wnsguddl789@gmail.com>
2023-03-19 20:13:06 +0900

    feat : core 컴포넌트 변경

--- src/core/Component.ts
+++ src/core/Component.ts
@@ -1,37 +1,39 @@
 export interface UnknownObject {
-  [key: string]: string | number | boolean;
+	[key: string]: string | number | boolean;
 }
 
 export default class Component<State> {
-  target!: HTMLElement;
-  protected state?: State;
-  constructor(target: HTMLElement, state: State) {
-    this.target = target;
-    this.initialState(state);
-  }
+	target!: HTMLElement;
+	state!: State;
+	protected element?: HTMLElement;
+	constructor(target: HTMLElement, state: State) {
+		this.target = target;
+		this.initialState(state);
+	}
 
-  protected initialState(initialState?: State) {
-    if (!initialState) {
-      this.render();
-      return;
-    }
-    this.setState(initialState);
-  }
+	protected initialState(initialState?: State) {
+		if (!initialState) {
+			this.render();
+			return;
+		}
+		this.setState(initialState);
+	}
 
-  protected componentDidMount() {}
+	protected componentDidMount() {}
 
-  protected setState(nextState: State) {
-    this.state = { ...this.state, ...nextState };
-    this.render();
-  }
+	protected setState(nextState: State) {
+		this.state = { ...this.state, ...nextState };
+		this.render();
+	}
 
-  protected template() {
-    return ``;
-  }
+	protected template() {
+		return ``;
+	}
 
-  protected render() {
-    this.target.innerHTML = this.template();
+	protected render() {
+		this.element = this.target;
+		this.element.innerHTML = this.template();
 
-    this.componentDidMount();
-  }
+		this.componentDidMount();
+	}
 }
