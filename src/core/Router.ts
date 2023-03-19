ba7dcb46f1c9d06769675f731014743d63d84ace
bbakjun <wnsguddl789@gmail.com>
2023-03-19 20:13:51 +0900

    feat : core 라우터 설정

--- src/core/Router.ts
+++ src/core/Router.ts
@@ -1,38 +1,43 @@
-import { HomeView } from '../views';
+import { HomeView, TrendView } from "../views";
 
 interface Routes {
-  path: string;
-  view: any;
+	path: string;
+	view: any;
 }
 
-const pathToRegex = (path: string) => new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
+const pathToRegex = (path: string) =>
+	new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
 const navigateTo = (url: string) => {
-  history.pushState(null, '', url);
-  router();
+	history.pushState(null, "", url);
+	router();
 };
 
 const router = async () => {
-  const routes: Routes[] = [{ path: '/', view: HomeView }];
+	const routes: Routes[] = [
+		{ path: "/", view: HomeView },
+		{ path: "/trend", view: TrendView },
+	];
 
-  // Test each route for potential match
-  const potentialMatches = routes.map((route) => ({
-    route: route,
-    result: location.pathname.match(pathToRegex(route.path)),
-  }));
+	// Test each route for potential match
+	const potentialMatches = routes.map((route) => ({
+		route: route,
+		result: location.pathname.match(pathToRegex(route.path)),
+	}));
 
-  let match = potentialMatches.find((potentialMatch) => potentialMatch.result !== null);
+	let match = potentialMatches.find((potentialMatch) => potentialMatch.result !== null);
 
-  if (!match) {
-    match = {
-      route: routes[0],
-      result: [location.pathname],
-    };
-  }
+	if (!match) {
+		match = {
+			route: routes[0],
+			result: [location.pathname],
+		};
+	}
 
-  const mainRoot = document.querySelector('main') as HTMLElement;
+	const mainRoot = document.querySelector("main") as HTMLElement;
+	console.log(match);
 
-  mainRoot.innerHTML = new match.route.view(mainRoot).template();
+	mainRoot.innerHTML = new match.route.view(mainRoot).template();
 };
 
 export { router, navigateTo };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import app from './app';
+import app from "./app";
+import { router, navigateTo } from "./core/Router";
 
-export const root = document.getElementById('app') as HTMLDivElement;
+window.addEventListener("popstate", router);
 
-document.addEventListener('DOMContentLoaded', () => new app(root));
+export const root = document.getElementById("app") as HTMLDivElement;
+
+document.addEventListener("DOMContentLoaded", () => {
+	navigateTo(window.location.pathname);
+	document.body.addEventListener("click", (e) => {
+		const target = e.target as HTMLAnchorElement;
+
+		if (target && target.matches("[data-link]")) {
+			e.preventDefault();
+			navigateTo(target.href);
+		}
+	});
+});
+
+new app(root);
